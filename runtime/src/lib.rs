011691752116a36c929b678d643d486b5bf2fe79
Cute_Wisp <sweatpotato13@gmail.com>
2023-02-17 11:15:40 +0900

    feat: 🎸 update to polkadot-v0.9.38

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -252,6 +252,7 @@ impl pallet_grandpa::Config for Runtime {
 
 	type WeightInfo = ();
 	type MaxAuthorities = ConstU32<32>;
+	type MaxSetIdSessionEntries = ();
 }
 
 impl pallet_timestamp::Config for Runtime {
@@ -774,6 +775,16 @@ impl_runtime_apis! {
 		) -> pallet_transaction_payment::FeeDetails<Balance> {
 			TransactionPayment::query_fee_details(uxt, len)
 		}
+		fn query_weight_to_fee(
+			weight: sp_weights::Weight,
+		) -> Balance {
+			TransactionPayment::weight_to_fee(weight)
+		}
+		fn query_length_to_fee(
+			len: u32,
+		) -> Balance {
+			TransactionPayment::length_to_fee(len)
+		}
 	}
 
 	impl pallet_transaction_payment_rpc_runtime_api::TransactionPaymentCallApi<Block, Balance, RuntimeCall>
@@ -790,6 +801,16 @@ impl_runtime_apis! {
 			len: u32,
 		) -> pallet_transaction_payment::FeeDetails<Balance> {
 			TransactionPayment::query_call_fee_details(call, len)
+		}
+		fn query_weight_to_fee(
+			weight: sp_weights::Weight,
+		) -> Balance {
+			TransactionPayment::weight_to_fee(weight)
+		}
+		fn query_length_to_fee(
+			len: u32,
+		) -> Balance {
+			TransactionPayment::length_to_fee(len)
 		}
 	}
 
